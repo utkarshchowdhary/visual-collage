b7c0759e6979beaf679963a379ebc43c00bb9b22
Utkarsh Chowdhary <utkarshchowdhary4140@gmail.com>
2020-11-15 00:10:46 +0530

    Updates

---
+++ precache-manifest.0b069c29a95c4b0737891c3225df771d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61342de5bf40caa836c9777928f02f2d",
+    "url": "/visual-collage/index.html"
+  },
+  {
+    "revision": "560210a8df8db9f18b9c",
+    "url": "/visual-collage/static/css/main.ab77e25f.chunk.css"
+  },
+  {
+    "revision": "4f9b3421446f4e8856df",
+    "url": "/visual-collage/static/js/2.230f2e3a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/visual-collage/static/js/2.230f2e3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "560210a8df8db9f18b9c",
+    "url": "/visual-collage/static/js/main.a354f271.chunk.js"
+  },
+  {
+    "revision": "6699bda13a8303fee0a9",
+    "url": "/visual-collage/static/js/runtime-main.4e4b507b.js"
+  }
+]);
