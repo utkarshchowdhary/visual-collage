9906f3087e81c635fbc6be53568a358282e8346f
Utkarsh Chowdhary <utkarshchowdhary4140@gmail.com>
2020-11-22 10:27:37 +0530

    pulled unsplash client id from code

--- src/apis/unsplash.js
+++ src/apis/unsplash.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
   baseURL: 'https://api.unsplash.com',
   headers: {
-    Authorization: 'Client-ID cl4v8AR2s9talMP5uRfe3t_fSlzpp0TkJI3GlEcK3zM',
+    Authorization: `Client-ID ${process.env.REACT_APP_UNSPLASH_CLIENT_ID}`,
   },
 });
